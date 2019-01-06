1a2b15d5d9c246e92cda6e6ee3f7f9a74fddf342
WaZeR [Adrien] <officielghostfire@gmail.com>
2019-01-06 14:35:12 +0100

    Add user group route

--- back/index.php
+++ back/index.php
@@ -18,37 +18,9 @@ $router->add('POST', '/auth', "Auth#check", "Authentication to the API");
 
 
 // Group of routes
-$router->group('/examples', function (Group $group) {
+$router->group('/users', function (Group $group) {
 
-    // Simple GET Method
-    $group->add('GET', '', "Example#getAll", "Get all Examples");
-
-    // GET Method with params
-    $group->add('GET', '/:slug-:id', "Example#index", "Index of Example", null, [], [
-        'slug' => 'String'
-    ]);
-
-    // PUT (UPDATE) Method with param
-    $group->add('PUT', '/:id', "Example#update", "Update an Example", true, [
-        '>' => 2 // Need the role superior to 2
-    ]);
-
-    // POST (INSERT) Method
-    $group->add('POST', '', "Example#add", "Add a new Example", true, [
-        '=' => 3 // Need the role equal to 3
-    ], [], [
-        '*email' => '',
-        '*password' => 'String',
-        'name' => 'String',
-        'age' => 'Int',
-    ]);
-
-    // DELETE Method
-    $group->add('DELETE', '/:id', "Example#delete", "Delete an Example", true, [
-        '>' => 4, // Need the role superior to 4
-        '<=' => 7 // Need the role inferior or equal to 7
-    ]);
-
+    
 
 }, null, [], [ // Params of routes in this group
     'id' => 'Int',
