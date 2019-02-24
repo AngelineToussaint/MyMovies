44a985161ba6abb00872d222a79dea190289c601
Adrien <officielghostfire@gmail.com>
2019-02-24 19:06:07 +0100

    First components and import ngx materialize

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {MembersComponent} from './members/members.component';
+
+const routes: Routes = [
+  {path: 'membres', component: MembersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MzButtonModule, MzCheckboxModule } from 'ngx-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MzButtonModule,
+    MzCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
