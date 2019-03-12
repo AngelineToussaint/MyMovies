39f1ab64e04e42a988f2d1c10c639701e37b50da
Adrien <adrienm.martineau@gmail.com>
2019-03-12 13:43:42 +0100

    Float and link for notes

--- front/src/app/sidebar/sidebar.component.ts
+++ front/src/app/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Note} from '../movie/note';
 import {NoteService} from '../movie/note.service';
 import {MovieService} from '../movie/movie.service';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,6 +12,7 @@ import {MovieService} from '../movie/movie.service';
 export class SidebarComponent implements OnInit {
 
   notes: Note[] = [];
+  pictureUrl: string = environment.pictureUrl;
 
   constructor(
     private noteService: NoteService,
@@ -24,7 +26,6 @@ export class SidebarComponent implements OnInit {
   getLastNotes(): void {
     this.noteService.getLast()
       .subscribe(notes => {
-        console.log(notes);
         for (const note of notes) {
           this.getMovie(note);
         }
