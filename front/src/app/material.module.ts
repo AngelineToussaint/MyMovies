2e39506d28cb33e6494bf43e7311707dc3dce2aa
Angéline <contact@angeline-toussaint.fr>
2019-03-14 20:23:19 +0100

    Add a design in movies

--- front/src/app/material.module.ts
+++ front/src/app/material.module.ts
@@ -1,23 +1,24 @@
 import {Input, NgModule} from '@angular/core';
-import {MatToolbarModule, MatListModule, MatIconModule, MatCardModule, MatTabsModule, MatGridListModule} from '@angular/material';
+import {
+  MatToolbarModule,
+  MatListModule,
+  MatIconModule,
+  MatCardModule,
+  MatTabsModule,
+  MatGridListModule,
+  MatButtonModule, MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    MatToolbarModule,
-    MatGridListModule,
-    MatListModule,
-    MatIconModule,
-    MatCardModule,
-    MatTabsModule
-  ],
   exports: [
     MatToolbarModule,
     MatGridListModule,
     MatListModule,
     MatIconModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class MaterialModule {
--- front/src/app/movies/movies.component.ts
+++ front/src/app/movies/movies.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Movie} from '../movie/movie';
 import {MovieService} from '../movie/movie.service';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
+
 export class MoviesComponent implements OnInit {
 
   movies: Movie[];
+  pictureUrl: string = environment.pictureUrl;
 
   constructor(private movieService: MovieService) { }
 
